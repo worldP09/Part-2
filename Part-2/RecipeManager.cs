278ac6863122bc06dee71a2fe0d604cb0ab31bb2
Phakamani  Stanley Nyoni <ST10381447@rcconnect.edu.za>
2024-05-11 05:59:35 +0200

    created  RecipeManager class to manage recipe-related operations.

---
+++ Part-2/RecipeManager.cs
@@ -0,0 +1,10 @@
+class RecipeManager 
+{
+    //list to store ricipes
+    private list<Recipe> recipes =new list<Recipe>();
+    //Store oringinal recipe for resetting quantities 
+    private List<Recipe> originalRecipes = new List<Recipe>();
+
+
+
+}
